2134cb0a491838ba2c854b43fed1c15a77ba96a2
dmolina <dmolina.reg@gmail.com>
2019-04-06 12:42:36 +0200

    Deleting all implementation

--- app/src/main/java/cat/dme/smart/marcopolo/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package cat.dme.smart.marcopolo.exceptions;
-
-/**
- * TODO: Explain class
- * Created by VIddA Software - DME Creaciones.
- */
-
-public class ValidationException extends Exception {
-}
